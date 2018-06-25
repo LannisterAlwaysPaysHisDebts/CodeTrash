636bfff99fabcdbc0a8c735169233f1a2d9e5dca
caoting <307298900@qq.com>
2018-06-25 12:09:10 +0800

    提交

--- practice/php/test.php
+++ practice/php/test.php
@@ -1,26 +1,7 @@
 <?php
-//*****while */
-$a = 1;
-$b = 2;
-do {
-    $a ++;
-    var_dump($b);
-}while($a <= 3 && $b < 1);
 
+$path = __DIR__ . '/file.txt';
 
-die();
-//*****分页 */
-    //分页大小
-    $pageSize = 50;
-    //数据
-    $data;
-    //计算页数
-    $pages = ceil($data / $pageSize);
-    //开始分页
-    for ($page = 0; $page < $pages; $page++) {
-        $start = $page * $pageSize;
-        $end = $pageSize + $start - 1;
-        
-        //分页的逻辑操作
-    }
+$data = 'aaaa' . "\r";
 
+file_put_contents($path, $data, FILE_APPEND);
