44c58758aa032b35bf81af69a1201ba52e6ccac4
caoting <307298900@qq.com>
2018-07-06 21:01:51 +0800

    thread edit

---
+++ practice/python/05_thread/01_create_thread.py
@@ -0,0 +1,19 @@
+# 使用_thread创建简单线程
+import _thread
+import time
+
+def print_time( threadName, delay):
+    count = 0
+    while count < 5:
+        time.sleep(delay)
+        count += 1
+        print("%s: %s" % (threadName, time.ctime(time.time()) ))
+
+try:
+    _thread.start_new_thread( print_time, ("Thread-1", 2))
+    _thread.start_new_thread( print_time, ("Thread-2", 4))
+except:
+    print("Error, cant start thread!")
+
+while 1:
+    pass
--- practice/python/07_mongo/01_import.py
+++ practice/python/07_mongo/01_import.py
@@ -2,4 +2,10 @@ import pymongo
 
 myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 
-mydb = myclient["testdb"]
+# 创建数据库testdb
+# mydb = myclient["testdb"]
+
+# 判断数据库是否存在
+# dblist = myclient.database_names()
+# print(dblist)
+
