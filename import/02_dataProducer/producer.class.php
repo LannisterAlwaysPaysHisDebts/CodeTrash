aa4c988d46c0e0c9b2bb8e6910e59cd8d40298ca
caoting <307298900@qq.com>
2018-07-06 14:37:04 +0800

    1

---
+++ import/02_dataProducer/producer.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 生产者
+ * 
+ * 暂定格式: 
+ * 数组:
+ *     key 字段名 
+ *      value 
+ */
+
+ class producer
+ {
+    public $data = [];
+
+    private $str_pool = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm";
+
+    private $num_pool = "1234567890";
+
+    
+
+     public function __construct()
+     {
+     }
+
+     public function producer()
+     {
+        
+     }
+
+
+ }
---
+++ import/02_dataProducer/ruler.class.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 制定者
+ */
+
+ require __DIR__ . '/producer.class.php';
+ require __DIR__ . '/worker.class.php';
+
+ class ruler
+ {
+    private $config;
+
+    public function __construct()
+    {
+        
+    }
+
+    
+    
+ }
