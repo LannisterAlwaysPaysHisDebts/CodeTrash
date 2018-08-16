519ddcae14ce7a1a55866f74a5662069e0d96270
caoting <307298900@qq.com>
2018-08-16 14:00:04 +0800

    生成器

--- php/practice/03_SPL/07_Generator.php
+++ php/practice/03_SPL/07_Generator.php
@@ -3,5 +3,20 @@
  * 生成器
  */
 
+function gen_one_to_three(){
+    for ($i = 1; $i <= 3; $i++) {
+        yield $i;
+    }
+}
+
+$generator = gen_one_to_three();
+
+// var_dump 给的是一个空对象
+var_dump($generator);
+
+// 打印出1, 2, 3
+foreach ($generator as $value) {
+    echo "$value\n";
+}
 
 
