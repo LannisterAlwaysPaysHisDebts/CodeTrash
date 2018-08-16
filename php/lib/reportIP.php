a4d46b622c95df6197ad2c3af97080a17efaf257
caoting <307298900@qq.com>
2018-08-16 15:59:59 +0800

    上报增加Ip.cn

--- php/lib/reportIP.php
+++ php/lib/reportIP.php
@@ -4,10 +4,13 @@
  */
 
 require dirname(dirname(__DIR__)) . '/vendor/autoload.php';
+require __DIR__ . '/ding.php';
 
 class reportIP
 {
     const IP_138 = 'http://2018.ip138.com/ic.asp';
+
+    const IP_CN = 'https://ip.cn/';
 
     private $obj;
 
@@ -46,7 +49,42 @@ class reportIP
         $string = substr($html, $start, $end - $start);
         $string = strip_tags($string);
 
-        $message = "服务器IP地址自动上报机器人: " . $string;
+        $message = "服务器IP地址自动上报机器人[138.com]: " . $string;
+        self::sendDing($message);
+
+        return true;
+    }
+
+    public function getIPByIpCn()
+    {
+        if (!class_exists('\QL\QueryList')) {
+            self::sendDing('抓取类初始化失败, 请检查脚本运行环境');
+            return false;
+        }
+
+        $rules = [
+            'ip' => ['.well p code', 'html']
+        ];
+
+        $loop = 0;
+        do{
+            $data = \QL\QueryList::get(self::IP_CN)->rules($rules)->query()->getData();
+            $loop ++;
+
+        }while(empty($data) && $loop <= 3);
+        $data = $data->all();
+
+        if (empty($data)) {
+            self::sendDing('重复3次抓取失败, 尝试切换其他方式');
+            return false;
+        }
+
+        $message = "服务器IP地址自动上报机器人[ip.cn]: ";
+        foreach ($data as $item) {
+            if (!empty($item['ip'])) {
+                $message .= $item['ip'];
+            }
+        }
         self::sendDing($message);
 
         return true;
@@ -57,3 +95,7 @@ class reportIP
         $this->obj->send($message);
     }
 }
+
+
+$obj = new reportIP();
+$obj->getIPByIpCn();
