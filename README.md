e2fa2d11600c9ccb64a5d9f0f6b304f264cfd3fe
caoting <307298900@qq.com>
2018-09-13 15:50:25 +0800

    删掉不必要的

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # myNotes
-Its my public note
-
-local访问地址:
-/Users/caoting/notes/myNotes/
+Practice!
--- php/lib/cronMan/cron.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * 入口文件
- */
-
-require __DIR__ . '/cronMan.class.php';
-
-$obj = new cronMan();
-$obj->start();
