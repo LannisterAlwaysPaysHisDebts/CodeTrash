02be8386755265d5edc754c318dcbc1989ec5778
caoting <307298900@qq.com>
2018-10-10 15:22:04 +0800

    胡改

---
+++ php/lib/tool/Tools.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tools
+ *
+ */
+
+namespace lib\tool;
+
+class Tools
+{
+    public static function start()
+    {
+        var_dump('111');
+    }
+}
---
+++ php/start.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * start from this
+ *
+ *
+ */
+
+namespace php;
+
+use lib\Load;
+
+require __DIR__ . '/lib/Load.php';
+Load::register();
