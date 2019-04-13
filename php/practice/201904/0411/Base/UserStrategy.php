68f7791bb09ae7052d2766ffb8da60c274a9d0b4
caoting <307298900@qq.com>
2019-04-13 23:40:27 +0800

    理解

--- php/practice/201904/0411/Base/UserStrategy.php
+++ php/practice/201904/0411/Base/UserStrategy.php
@@ -38,6 +38,16 @@ class Page
     }
 }
 
+/**
+ * 这里是使用的依赖注入???
+ * 就是Page类不用关心UserStrategy类传进来的是一个什么样具体的实现(是男生还是女生)
+ * 只需要调用UserStrategy接口固定实现的两个方法: showAd和showCategory
+ *
+ * 这种情况我能想象的一个好处就是在最外层获取到用户性别数据的时候就实例化了对应的对象,而后所有的逻辑都不用进行修改,
+ * 比如在下面这个逻辑里面增加了一个儿童用户, 只需要新建立一个ChildUserStrategy类,
+ * 然后在最外层获取数据的地方加一个if (type == 'Child'){ new ChildUserStrategy, 而后所有的方法都不用动
+ *
+ */
 $page = new Page();
 if ($_GET['Female']) {
     $strategy = new FemaleUserStrategy();
